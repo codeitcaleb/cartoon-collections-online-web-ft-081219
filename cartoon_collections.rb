226ee95c623841417938d9c483ea230d7653d211
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 18:52:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(calls_short)
 end
 
 def find_the_cheese(ingredients)
-  ingredients.map.include?("cheddar", "gouda", "camembert") 
+  ingredients.map.include?["cheddar", "gouda", "camembert"] 
   binding.pry
   # {|cheese| cheese ? puts "#{cheese}" : nil}
 end
