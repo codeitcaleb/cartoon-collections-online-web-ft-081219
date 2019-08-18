54624dc87217d06f7ddef3d5fca92cc6d0f272e2
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 17:31:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,16 @@
 require 'pry'
 
 def roll_call_dwarves(dwarves)
-dwarves.each_with_index {|dwarf, index| puts "#{index + 1}.#{dwarf}"}
+  dwarves.each_with_index {|dwarf, index| puts "#{index + 1}.#{dwarf}"}
 end
 
-
-
 def summon_captain_planet(planeteer_calls)
-
-planeteer_calls.map {|elements| "#{elements.capitalize}!"}
-
+  planeteer_calls.map {|elements| "#{elements.capitalize}!"}
 end
 
 def long_planeteer_calls()
-  # Your code here
+  
+  
 end
 
 def find_the_cheese()
