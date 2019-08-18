53b2c44a80e308ff8bb8437de5c45af46cf1300d
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 17:29:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+dwarves.each_with_index {|dwarf, index| puts "#{index + 1}.#{dwarf}"}
+end
+
+
+
+def summon_captain_planet(planeteer_calls)
+
+planeteer_calls.map {|elements| "#{elements.capitalize}!"}
+
+end
+
+def long_planeteer_calls()
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
