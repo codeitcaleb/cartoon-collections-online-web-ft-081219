55fd35befef058a6d405312207b27cbe859dcbff
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 19:19:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,14 +4,16 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, index| puts "#{index + 1}.#{dwarf}"}
 end
 
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|elements| "#{elements.capitalize}!"}
 end
 
+
 def long_planeteer_calls(calls_short)
  calls_short.map.any? {|calls| calls.length > 4 }
-  
 end
+
 
 def find_the_cheese(ingredients)
   
